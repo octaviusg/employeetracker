43c60397393ba8da501e6c273d0d52322ca3fbc7
Octavio Garcia <octaviogarciapc@gmail.com>
2020-10-19 19:58:28 -0500

    constructor and employee table

---
+++ app.js
@@ -0,0 +1,49 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+
+
+class Database {
+    constructor(config) {
+        this.connection = mysql.createConnection(config);
+    }
+
+    query(sql, args) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(sql, args, (err, rows) => {
+                if (err)
+                    return reject(err);
+                resolve(rows);
+            });
+        });
+    }
+
+    close() {
+        return new Promise((resolve, reject) => {
+            this.connection.end(err => {
+                if (err)
+                    return reject(err);
+                resolve();
+            });
+        });
+    }
+}
+
+const db = new Database({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Mamiya5369",
+    database: "employeeDB"
+});
+
+//show all employees
+async function showEmployeeSummary() {
+    console.log(' ');
+    await db.query('SELECT e.id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runApp();
+    });
+};
